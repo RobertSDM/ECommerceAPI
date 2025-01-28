79abfc18cceef50a542671457c48b46d5861fae2
Roberto <roberto.menesantos@gmail.com>
2025-01-28 10:56:10 -0300

    feat: project creation

---
+++ src/main/java/ponto/ecommerce/api/EcommerceApiApplication.java
@@ -0,0 +1,15 @@
+package ponto.ecommerce.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class EcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApiApplication.class, args);
+	}
+
+}
